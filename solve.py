2d4ec4051ce9d0c081959f4802e0eeafba580831
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2014-12-29 21:49:50 +0000

    Functions no longer access global variables; wrap vars together.

--- solve.py
+++ solve.py
@@ -1,7 +1,23 @@
+import collections
+import itertools
+
 import termcolor
+
+State = collections.namedtuple('State', ('grid', 'score', 'used', 'cx', 'cy'))
+Cell = collections.namedtuple('Cell', ('letter', 'min'))
+WordTree = collections.namedtuple('WordTree', ('is_word', 'children'))
+Candidate = collections.namedtuple('Candidate', ('word', 'ps', 'removed', 'score'))
 
 words = set(w.upper() for w in open('sowpods.txt').read().split())
 words = sorted(words)
+full_word_tree = {}
+for word in words:
+    word_tree = full_word_tree
+    last = len(word) - 1
+    for i, ch in enumerate(word):
+        if ch not in word_tree:
+            word_tree[ch] = WordTree(i == last, {})
+        word_tree = word_tree[ch].children
 
 values = {
     'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1,
@@ -11,72 +27,62 @@ values = {
 }
 special = {'J', 'Q', 'X', 'Z'}
 
-full_word_tree = {}
-for word in words:
-    word_tree = full_word_tree
-    last = len(word) - 1
-    for i, ch in enumerate(word):
-        if ch not in word_tree:
-            word_tree[ch] = (i == last, {})
-        word_tree = word_tree[ch][1]
-
 grid = """
-totbaiat
-daoia.te
-oedvwebu
-pyerfutn
-ehmuzkln
-jafyierx
-siclisir
-sgomniow
-cee.aods
-nqahlung
-og..ated
-pirvlsre
+       d
+       y
+       f
+       a
+       o
+o      i
+t      o
+n      o
+o b  o j
+uftawrlo
+igwnlahl
 """.strip('\n')
 
-min_lengths = """\
+mins = """\
 """
 
-def get(g, x, y):
-    """
-    Find a character from the original grid.
-    """
-    try:
-        return g[y][x].strip()
-    except IndexError:
-        return ''
+def state_from_text(letters, mins='', score=0, used=()):
+    grid = {}
+    cs = {y: {x: c for x, c in enumerate(row)} for y, row in enumerate(letters.upper().split('\n'))}
+    ms = {y: {x: c for x, c in enumerate(row)} for y, row in enumerate(mins.split('\n'))}
+    cx, cy = max(map(len, cs.values())), len(cs)
+    print cx, cy
+    for x in range(cx):
+        for y in range(cy):
+	    c = cs[y].get(x, '').strip()
+	    m = int(ms.get(y, {}).get(x, 0))
+	    grid[(x, y)] = Cell(c, m)
+    return State(grid, score, used, cx, cy)
 
-grid = tuple(tuple(l) for l in grid.upper().split('\n'))
-min_lengths = tuple(tuple(l) for l in min_lengths.split('\n'))
+state = state_from_text(grid, mins)
 
-cx, cy = max(map(len, grid)), len(grid)
 
-grid = dict(((x,y), get(grid, x, y)) for x in range(cx) for y in range(cy))
-min_lengths = dict(((x,y), int(get(min_lengths, x, y) or '0')) for x in range(cx) for y in range(cy))
-
-def print_grid(grid, ps=(), removed_tiles=()):
-    for y in range(cy):
+def print_state(state, candidate=None):
+    for y in range(state.cy):
         print termcolor.colored(str(y).rjust(2), 'grey') + " ",
-        for x in range(cx):
+        for x in range(state.cx):
             p = x, y
-	    if (x, y) in ps:
+	    if candidate and (x, y) in candidate.ps:
 	        color = 'red'
-	    elif (x, y) in removed_tiles:
+	    elif candidate and (x, y) in candidate.removed:
 	        color = 'blue'
 	    else:
 	        color = None
-            print termcolor.colored((grid[p] or ' ') + str(min_lengths[p] or ' '), color),
+	    cell = state.grid[p]
+            print termcolor.colored((cell.letter or ' ') + str(cell.min or ' '), color),
         print
 
 
 
-def surrounding_cells(p):
+def surrounding_cells(state, p):
     """
     Returns co-ordinates for all cells adjacent to the one given.
     """
     x, y = p
-    return set(filter(on_board, {
+    return set(p for p in {
         (x-1, y-1),
         (x-1, y  ),
         (x-1, y+1),
@@ -85,27 +91,27 @@ def surrounding_cells(p):
         (x+1, y-1),
         (x+1, y  ),
         (x+1, y+1)
-    }))
+    } if on_board(state, p))
 
-def adjacent_cells(p):
+def adjacent_cells(state, p):
     """
     Returns co-ordinates for all cells adjacent to the one given.
     """
     x, y = p
-    return set(filter(on_board, {
+    return set(p for p in {
         (x-1, y  ),
         (x  , y-1),
         (x  , y+1),
         (x+1, y  ),
-    }))
+    } if on_board(state, p))
 
-def all_cells():
-    return {(x, y) for x in range(cx) for y in range(cy)}
+def all_cells(state):
+    return {(x, y) for x in range(state.cx) for y in range(state.cy)}
 
-def on_board(p):
-    return 0 <= p[0] < cx and 0 <= p[1] < cy
+def on_board(state, p):
+    return 0 <= p[0] < state.cx and 0 <= p[1] < state.cy
 
-def get_moves(grid, word_tree, used):
+def get_moves(state, word_tree, visited):
     """
     Work out what moves can be made.
 
@@ -113,36 +119,38 @@ def get_moves(grid, word_tree, used):
     visited.  Returns [((is_word, word_tree), p)], where p is the co-ordinates
     of a cell for each possible move.
     """
-    if used:
-        possible = surrounding_cells(used[-1])
+    if visited:
+        possible = surrounding_cells(state, visited[-1])
     else: # We can start anywhere
-        possible = all_cells()
+        possible = all_cells(state)
 
     # Filter out the ones that aren't possible
     possible = {(px, py) for (px, py) in possible
-                   if (px, py) not in used and           # Haven't used this letter before
-                      grid[(px, py)] in word_tree}       # We're on the way to making an actual word
+                   if (px, py) not in visited and          # Haven't used this letter before
+                      state.grid[(px, py)].letter in word_tree}   # We're on the way to making an actual word
 
-    return [(word_tree[grid[p]], p) for p in possible]
+    return [(word_tree[state.grid[p].letter], p) for p in possible]
     
-def seek(grid, word, word_tree, used, found, min_length):
-    moves = get_moves(grid, word_tree, used)
+def seek(state, candidates, word_tree=full_word_tree, word='', min_length=3, visited=()):
+    moves = get_moves(state, word_tree, visited)
     for (is_word, new_word_tree), p in moves:
-        new_used = used + (p,)
-        new_word = word + grid[p]
-        new_min_length = max(min_length, min_lengths[p])
-        if is_word and len(new_word) >= new_min_length:# and len([1 for x,y in used if x in (7,)]) > 2:
-            found.append((new_word, new_used))
-        seek(grid, new_word, new_word_tree, new_used, found, new_min_length)
+        new_visited = visited + (p,)
+        new_word = word + state.grid[p].letter
+        new_min_length = max(min_length, state.grid[p].min)
+        if is_word and len(new_word) >= new_min_length and new_word not in state.used:
+	    removed_tiles = get_removed_tiles(state, new_word, new_visited)
+	    score =  get_score(state, new_visited, removed_tiles)
+	    candidates.append(Candidate(new_word, new_visited, frozenset(removed_tiles), score))
+        seek(state, candidates, new_word_tree, new_word, new_min_length, new_visited)
 
-def get_removed_tiles(grid, ps):
+def get_removed_tiles(state, word, ps):
     removed = list(ps)
     for i, p in enumerate(ps):
-        adjacent = adjacent_cells(p)
+        adjacent = adjacent_cells(state, p)
         if len(word) < 5:
-	    adjacent = set(p for p in adjacent if grid[p] == '.')
-	if grid[p] in special:
-	    adjacent |= set((px, p[1]) for px in range(0, cx) if px != p[0])
+	    adjacent = set(p for p in adjacent if state.grid[p].letter == '.')
+	if state.grid[p].letter in special:
+	    adjacent |= set((px, p[1]) for px in range(0, state.cx) if px != p[0])
 
 	if i > 0:
 	    adjacent.discard(ps[i-1])
@@ -151,50 +159,36 @@ def get_removed_tiles(grid, ps):
         removed.extend(adjacent)
     return removed
 
-def get_score(grid, ps, removed_tiles):
-    return len(ps) * sum(values[grid[r]] for r in removed_tiles)
+def get_score(state, ps, removed_tiles):
+    return len(ps) * sum(values[state.grid[r].letter] for r in removed_tiles)
 
-def get_new_grid(grid, removed):
+def get_new_grid(state, candidate):
     new_grid = {}
-    for x in range(cx):
-        y, oy = cy - 1, cy - 1
+    for x in range(state.cx):
+        y, oy = state.cy - 1, state.cy - 1
 	while y >= 0:
-	    if (x, oy) not in removed:
-		new_grid[(x, y)] = grid.get((x, oy), '')
+	    if (x, oy) not in candidate.removed:
+		new_grid[(x, y)] = state.grid.get((x, oy), Cell('', 0))
 	        y -= 1
 	    oy -= 1
-    return new_grid
+    return State(new_grid,
+                 state.score + candidate.score,
+		 state.used + (candidate.word,),
+		 state.cx, state.cy)
 
-found = []
-seek(grid, '', full_word_tree, (), found, 3)
+candidates = []
+seek(state, candidates) 
 
-new_found = []
-for word, ps in found:
-    removed_tiles = get_removed_tiles(grid, ps)
-    score = get_score(grid, ps, removed_tiles)
-    new_found.append((word, ps, score, removed_tiles))
-found = new_found
+candidates.sort(key=lambda c: (c.score, -len(c.word), c.word))
 
-#import sys
-#found = [f for f in found if f[0]==sys.argv[1].upper()]
+for candidate in candidates[-20:]:
+    print str(candidate.score).rjust(4), candidate.word
 
-found = sorted(found, key=lambda (word, ps, score, removed_tiles): (score, -len(word), word, ps))
+print len(candidates)
 
-#found_words = [(word, ps) for (word, ps) in found_words]
-
-for word, ps, score, removed_tiles in found[-20:]:
-    print str(score).rjust(4), word, ps, removed_tiles
-
-from collections import defaultdict
-counts = defaultdict(int)
-for p in removed_tiles:
-    counts[p] += 1
-
-print len(found)
-
-counts = sorted(counts.items(), key=lambda c:c[-1])
-print counts
-
-print_grid(grid, ps, removed_tiles)
+print_state(state)
 print
-print_grid(get_new_grid(grid, set(removed_tiles)))
+print_state(state, candidates[-1])
+print
+print_state(get_new_grid(state, candidates[-1])
+
